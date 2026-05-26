47830205ba0dad5b31c7add76e23aa3dda5768bc
Vadim Vasiliev <overherz@gmail.com>
2026-05-27 00:45:41 +0400

    Reorder categories

--- models/Category.py
+++ models/Category.py
@@ -7,15 +7,15 @@ class Category:
     BLACKLIST = ['4k']
 
     STATIC_CATEGORIES = [
+        lambda: {'id': 'search', 'title': 'Поиск', 'icon': 'search', 'path': '/msx/search', 'params': {'q': '{INPUT}'}, 'interaction': 'http://msx.benzac.de/interaction/input.html', 'options': 'search:3|ru|Поиск'},
+        lambda: {'id': 'bookmarks', 'title': 'Закладки', 'icon': 'bookmark', 'path': '/msx/bookmarks'},
+        lambda: {'id': 'history', 'title': 'История', 'icon': 'history', 'path': '/msx/history', 'params': {'page': '{PAGE}'}, 'interaction': f'{config.MSX_HOST}/paging.html'},
+        lambda: {'id': 'watching', 'title': 'Я смотрю', 'icon': 'tv', 'path': '/msx/watching'},        
         lambda: {'id': 'new', 'title': 'Новинки', 'path': '/msx/category', 'params': {'sort': 'created-', 'page': '{PAGE}'}, 'interaction': f'{config.MSX_HOST}/paging.html'},
         lambda: {'id': 'toons', 'title': 'Мультфильмы', 'path': '/msx/category', 'params': {'genre': '23', 'page': '{PAGE}'}, 'interaction': f'{config.MSX_HOST}/paging.html'},
         lambda: {'id': 'anime', 'title': 'Аниме', 'path': '/msx/category', 'params': {'genre': '25', 'page': '{PAGE}'}, 'interaction': f'{config.MSX_HOST}/paging.html'},
         lambda: {'id': 'collections', 'title': 'Подборки', 'path': '/msx/collections', 'params': {'page': '{PAGE}'}, 'interaction': f'{config.MSX_HOST}/paging.html'},
         lambda: {'id': 'sport', 'title': 'Спорт', 'path': '/msx/tv'},
-        lambda: {'id': 'search', 'title': 'Поиск', 'icon': 'search', 'path': '/msx/search', 'params': {'q': '{INPUT}'}, 'interaction': 'http://msx.benzac.de/interaction/input.html', 'options': 'search:3|ru|Поиск'},
-        lambda: {'id': 'bookmarks', 'title': 'Закладки', 'icon': 'bookmark', 'path': '/msx/bookmarks'},
-        lambda: {'id': 'history', 'title': 'История', 'icon': 'history', 'path': '/msx/history', 'params': {'page': '{PAGE}'}, 'interaction': f'{config.MSX_HOST}/paging.html'},
-        lambda: {'id': 'watching', 'title': 'Я смотрю', 'icon': 'tv', 'path': '/msx/watching'},
         lambda: {'id': 'settings', 'title': 'Настройки', 'icon': 'settings', 'path': '/msx/settings/screen'}
     ]
 
